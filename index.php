98800af0efb36e15373bebd73e8e1c4e9ca50c2d
karfaisarra <karfaisarra@gmail.com>
2022-12-07 16:26:19 +0100

    add objects

--- index.php
+++ index.php
@@ -3,17 +3,25 @@
 class Movie
 {
     public $nameMovie;
+    public $year;
     public $description;
     public $duration;
 
-    public function  __construct($nameMovie, $description, $duration)
+    public function  __construct($nameMovie, $year, $description, $duration)
     {
         $this->nameMovie = $nameMovie;
+        $this->year = $year;
         $this->description = $description;
         $this->duration = $duration;
     }
     public function getInfoMovie()
     {
-        return "Nome: $this->nameMovie <br> Descrizione: $this->description <br> Durata: $this->duration  ";
+        return "<br> Nome: $this->nameMovie <br> Anno: $this->year <br> Descrizione: $this->description <br> Durata: $this->duration  ";
     }
 }
+
+$movie_1 = new Movie('The Irishman', 2019, 'Drama & Crime Movie', '3h 29m');
+echo $movie_1->getInfoMovie();
+
+$movie_2 = new Movie('Troll', 2022, 'Fantasy Movie', '1h 41m');
+echo $movie_2->getInfoMovie();
